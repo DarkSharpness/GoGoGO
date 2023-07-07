63d7414a8c1b1298cfd288727bab0c3180c9b457
DarkSharpness_XzY <2040703891@qq.com>
2023-07-07 12:00:59 +0800

    Pass all test points

--- src/main.go
+++ src/main.go
@@ -11,7 +11,7 @@ import (
 )
 
 func main() {
-	server, err := net.Listen("tcp", ":1080")
+	server, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		fmt.Printf("Listen failed: %v\n", err)
 		return
@@ -48,28 +48,39 @@ func process(client net.Conn) {
 func Socks5_Auth(client net.Conn) error {
 	var buf [512]byte
 
-	// Read in first 2 byte of header information.
-	n, err := client.Read(buf[:2])
-	if n != 2 {
-		return errors.New("Header Error " + err.Error())
+	_, err := io.ReadFull(client, buf[:2])
+	if err != nil {
+		return errors.New("read Error: " + err.Error())
 	}
 
 	// Check the version (Socks 5 only)
 	ver, Nmethod := int(buf[0]), int(buf[1])
 	if ver != 0x05 {
-		return errors.New("Invalid Version!")
+		return errors.New("invalid Version!")
 	}
 
 	// Read in the method data.
-	n, err = client.Read(buf[:Nmethod])
-	if Nmethod != n {
-		return errors.New("Method length mismatch!")
+	_, err = io.ReadFull(client, buf[:Nmethod])
+	if err != nil {
+		return errors.New("read Error: " + err.Error())
+	}
+
+	flag := true
+	for i := 0; i != Nmethod; i++ {
+		if buf[i] == 0x00 {
+			flag = false
+			break
+		}
+	}
+	if flag == true {
+		client.Write([]byte{0x05, 0xff})
+		return errors.New("no acceptable methods.")
 	}
 
 	// We choose 0x00 method
-	n, err = client.Write([]byte{0x05, 0x00})
-	if n != 2 || err != nil {
-		return errors.New("Write error" + err.Error())
+	_, err = client.Write([]byte{0x05, 0x00})
+	if err != nil {
+		return errors.New("write error: " + err.Error())
 	}
 
 	return nil
@@ -79,46 +90,56 @@ func Socks5_Connect(client net.Conn) (net.Conn, error) {
 	var buf [512]byte
 
 	// Read in first 4 byte of header information.
-	n, err := client.Read(buf[:4])
-	if n != 4 {
+	_, err := io.ReadFull(client, buf[:4])
+	if err != nil {
 		return nil, errors.New("Header Error " + err.Error())
 	}
 
+	// The error bytes to return when error occured.
+	error_buffer := []byte{0x05, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00}
+
 	// Check version,cmd and reservation
 	ver, cmd, rsv, atyp := int(buf[0]), int(buf[1]), int(buf[2]), int(buf[3])
-	if ver != 0x05 || cmd == 0 || rsv != 0x00 {
-		return nil, errors.New("Invalid ver/cmd/rsv!")
+
+	if ver != 0x05 || cmd != 1 || rsv != 0x00 {
+		error_buffer[1] = 0x07
+		client.Write(error_buffer)
+		return nil, errors.New("invalid ver/cmd/rsv!")
 	}
 
 	// Get the full string and the buffer first
-	n, err = client.Read(buf[:])
-	if n < 2 {
-		return nil, errors.New("Invalid address!" + err.Error())
-	}
+	port := uint16(0)
+	addr := string("")
 
-	// port number from 0 ~ 65535.
-	port := binary.BigEndian.Uint16(buf[n-2 : n])
-	addr := ""
-
-	// Check the atyp
+	// Check the atyp first
 	switch atyp {
 	case 0x01: // ipv4 case
-		if n != 6 {
-			return nil, errors.New("Invalid ipv4 address!")
+		_, err = io.ReadFull(client, buf[:6])
+		if err != nil {
+			return nil, errors.New("read error!" + err.Error())
 		}
+		port = binary.BigEndian.Uint16(buf[4:6])
 		addr = fmt.Sprintf("%d.%d.%d.%d:%d", buf[0], buf[1], buf[2], buf[3], port)
 		break
 
 	case 0x03: // domain case
-		if n != int(buf[0])+3 {
-			return nil, errors.New("Invalid domain!")
+		_, err = io.ReadFull(client, buf[:1])
+		if err != nil {
+			return nil, errors.New("read error!" + err.Error())
 		}
-		addr = string(buf[1:n-2]) + fmt.Sprintf(":%v", port)
+		len := int(buf[0]) + 2
+		_, err = io.ReadFull(client, buf[:len])
+		if err != nil {
+			return nil, errors.New("read error!" + err.Error())
+		}
+		port = binary.BigEndian.Uint16(buf[len-2 : len])
+		addr = string(buf[0:len-2]) + fmt.Sprintf(":%v", port)
 		break
 
 	case 0x04: // ipv6 case
-		if n != 18 {
-			return nil, errors.New("Invalid ipv6 address!")
+		_, err = io.ReadFull(client, buf[:18])
+		if err != nil {
+			return nil, errors.New("read error!" + err.Error())
 		}
 		addr = "["
 		addr += fmt.Sprintf("%x:", binary.BigEndian.Uint16(buf[0:2]))
@@ -129,53 +150,67 @@ func Socks5_Connect(client net.Conn) (net.Conn, error) {
 		addr += fmt.Sprintf("%x:", binary.BigEndian.Uint16(buf[10:12]))
 		addr += fmt.Sprintf("%x:", binary.BigEndian.Uint16(buf[12:14]))
 		addr += fmt.Sprintf("%x", binary.BigEndian.Uint16(buf[14:16]))
-		addr += fmt.Sprintf("]:%d", port)
+		addr += fmt.Sprintf("]:%d", binary.BigEndian.Uint16(buf[16:18]))
 		break
 
-	default: // Error case
-		return nil, errors.New("Invalid aytp!")
+	default:
+		error_buffer[1] = 0x08
+		client.Write(error_buffer)
+		return nil, errors.New("invalid address!")
 	}
 
 	// Tries to dial the address
 	if atyp == 0x03 {
 		fmt.Printf("Website domain: %v\n", addr)
 	} else if atyp == 0x01 {
-		fmt.Println("Website ipv4: %v\n", addr)
+		fmt.Printf("Website ipv4: %v\n", addr)
 	} else {
-		fmt.Println("Website ipv6: %v\n", addr)
+		fmt.Printf("Website ipv6: %v\n", addr)
 	}
 
 	dest, err := net.Dial("tcp", addr)
 	if err != nil {
-		client.Write([]byte{0x05, 0x05, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
-		return nil, err
+		code := byte(0x00)
+		// error string shortcut
+		str := err.Error()
+		if strings.Contains(str, "no route") {
+			code = 0x03
+		} else if strings.Contains(str, "lookup") {
+			code = 0x04
+		} else if strings.Contains(str, "network is unreachable") {
+			code = 0x03
+		} else if strings.Contains(str, "name resolution") {
+			code = 0x04
+		} else if strings.Contains(str, "refused") {
+			code = 0x05
+		}
+		error_buffer[1] = code
+		client.Write(error_buffer)
+		return nil, errors.New("dial failure: " + str)
 	}
 
-	IP_str, port_str, err := net.SplitHostPort(dest.LocalAddr().String())
+	IP_str, port_str, _ := net.SplitHostPort(dest.LocalAddr().String())
 	IP := net.ParseIP(IP_str)
 	port_32, _ := strconv.Atoi(port_str)
 	port = uint16(port_32)
 
-	if atyp == 0x03 {
-		if strings.Contains(IP_str, ".") {
-			IP = IP[12:]
-			atyp = 0x01
-		} else {
-			atyp = 0x04
-		}
+	if len(IP) == 4 {
+		atyp = 0x01
+	} else {
+		atyp = 0x04
 	}
 
-	rvl := []byte{0x05, 0x00, 0x00, byte(atyp)}
-	rvl = append(rvl, IP...)
+	rvl := append([]byte{0x05, 0x00, 0x00, byte(atyp)}, IP...)
 
-	_, err = client.Write(binary.BigEndian.AppendUint16([]byte(rvl), port))
-	fmt.Printf("Local IP: %v Port: %v\n", IP, port)
+	//  Debug use only
+	fmt.Printf("Local IP: %v New Port: %v\n", IP, port)
+	fmt.Printf("Debug: %v\n", binary.BigEndian.AppendUint16(rvl, port))
 
+	_, err = client.Write(binary.BigEndian.AppendUint16(rvl, port))
 	if err != nil {
 		dest.Close()
 		return nil, err
 	}
-
 	return dest, nil
 }
 
